64d1f88a3054aec859b915e0ffabce9f6ce9beef
Konrad Majewski <km371194@students.mimuw.edu.pl>
2017-01-04 19:52:19 +0100

    Citizen - header file and implementation

---
+++ src/citizen.cc
@@ -0,0 +1,34 @@
+// Konrad Majewski, Mateusz Warzyñski
+#include <cassert>
+#include "citizen.h"
+
+Citizen::Citizen(HealthPoints healthPoints, Age age) 
+        : Living(healthPoints), age_(age) {}
+
+Age Citizen::getAge() {
+    return age_;
+}
+
+Adult::Adult(HealthPoints healthPoints, Age age) : Citizen(healthPoints, age) {
+    assert(18 <= age && age <= 100);
+}
+
+Teenager::Teenager(HealthPoints healthPoints, Age age) 
+        : Citizen(healthPoints, age) {
+    assert(11 <= age && age <= 17);
+}
+
+Sheriff::Sheriff(HealthPoints healthPoints, Age age, AttackPower attackPower)
+        : Citizen(healthPoints, age), Attacking(attackPower) {}
+
+Adult createAdult(HealthPoints healthPoints, Age age) {
+    return Adult(healthPoints, age);
+}
+
+Teenager createTeenager(HealthPoints healthPoints, Age age) {
+    return Teenager(healthPoints, age);
+}
+
+Sheriff createSheriff(HealthPoints healthPoints, Age age, AttackPower attackPower) {
+    return Sheriff(healthPoints, age, attackPower);
+}
--- src/citizen.h
+++ src/citizen.h
@@ -1,57 +1,38 @@
+// Konrad Majewski, Mateusz Warzyński
 #ifndef HORROR_CITIZEN_H
 #define HORROR_CITIZEN_H
 
-#include <cassert>
+#include <cstdint>
+#include "helper.h"
 
-template<typename Age, typename HealthPoints, typename AttackPower>
-class Citizen {
-    HealthPoints healthPoints;
-    Age age;
+using Age = int32_t;
 
-public:
+class Citizen : Living {
+  public:
+    Citizen(HealthPoints healthPoints, Age age);
+    Age getAge();
 
-    Citizen(HealthPoints healthPoints, Age age) : healthPoints(healthPoints), age(age) {}
-
-    HealthPoints getHealth() { return healthPoints; }
-
-    Age getAge() { return age; }
-
-    void takeDamage(AttackPower damage) {
-        healthPoints -= damage;
-        if (healthPoints < 0)
-            healthPoints = 0;
-    }
+  private:
+    Age age_;
 };
 
-template<typename Age, typename HealthPoints, typename AttackPower>
-class Adult : public Citizen<Age, HealthPoints, AttackPower> {
-public:
-
-    Adult(HealthPoints healthPoints, Age age) : Citizen<Age, HealthPoints, AttackPower>(healthPoints, age) {
-        assert(100 >= age && age >= 18);
-    }
+class Adult : public Citizen {
+  public:
+    Adult(HealthPoints healthPoints, Age age);
 };
 
-template<typename Age, typename HealthPoints, typename AttackPower>
-class Teenager : public Citizen<Age, HealthPoints, AttackPower> {
-public:
-
-    Teenager(HealthPoints healthPoints, Age age) : Citizen<Age, HealthPoints, AttackPower>(healthPoints, age) {
-        assert(17 >= age && age >= 11);
-    }
+class Teenager : public Citizen {
+  public:
+    Teenager(HealthPoints healthPoints, Age age);
 };
 
-template<typename Age, typename HealthPoints, typename AttackPower>
-class Sheriff : public Adult<Age, HealthPoints, AttackPower> {
-    AttackPower attackPower;
-
-public:
-
-    Sheriff(HealthPoints healthPoints, Age age, AttackPower attackPower) : Adult<Age, HealthPoints, AttackPower>(
-            healthPoints, age), attackPower(attackPower) {}
-
-    AttackPower getAttackPower() { return attackPower; }
+class Sheriff : public Citizen, public Attacking {
+  public:
+    Sheriff(HealthPoints healthPoints, Age age, AttackPower attackPower);
 };
 
+Adult createAdult(HealthPoints healthPoints, Age age);
+Teenager createTeenager(HealthPoints healthPoints, Age age);
+Sheriff createSheriff(HealthPoints healthPoints, Age age, AttackPower attackPower);
 
 #endif //HORROR_CITIZEN_H
